6d7b6429f70f82e4372d7ee644524804dc261977
Rita Sukhova <nadobnomars@gmail.com>
2019-02-21 18:17:54 +0300

    Добавлены константы для слоя бизнес логики

---
+++ Watcher/Watcher/Classes/Business Logic/Common/Constants/BusinessLogicConstants.swift
@@ -0,0 +1,13 @@
+//
+//  BusinessLogicConstants.swift
+//  Watcher
+//
+//  Created by Маргарита on 21/02/2019.
+//  Copyright © 2019 Маргарита. All rights reserved.
+//
+
+import Foundation
+
+//struct <#name#> {
+//    <#fields#>
+//}
