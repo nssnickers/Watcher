feb78d5b240602db5a92f1a1dc93dfd653a4236a
Rita Sukhova <nadobnomars@gmail.com>
2019-02-22 11:17:35 +0300

    Добавлена документация к константам

--- Watcher/Watcher/Classes/Presentation/Common/Constants/PresentationConstants.swift
+++ Watcher/Watcher/Classes/Presentation/Common/Constants/PresentationConstants.swift
@@ -9,29 +9,59 @@
 import Foundation
 import UIKit
 
+
+/// Константы валидации
 struct Validation {
+    
+    /// Описание ошибок валидации
     struct ErrorDescription {
+        
+        /// Короткий пароль
         static let shortPassword = NSLocalizedString("Слишком короткий пароль", comment: "")
+        
+        /// Неверный формат логина
         static let invalidLogin = NSLocalizedString("Неверный формат логина", comment: "")
     }
     
+    
+    /// Правила валидации
     struct Rules {
+        
+        /// Паттерн валидации email
         static let emailPattern = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        /// Формат валидации email
         static let emailValidationFormat = "SELF MATCHES %@"
+        
+        /// Минимальная длинна пароля
         static let passwordMinLength = 6
     }
 }
 
+
+/// Цвета
 struct Colors {
+    
+    /// Красный
     static let red = UIColor(named: "orangeyRed")
+    
+    /// Приглушенный красный
     static let pastelRed = UIColor(named: "pastelOrangeyRed")
 }
 
+
+/// Константы для Alert
 struct Alert {
+    
+    /// Заголовок
     static let title = NSLocalizedString("Внимание", comment: "")
+    
+    /// Название действия
     static let actionTitle = NSLocalizedString("ОК", comment: "")
 }
 
+
+/// Длительность анимаций
 struct AnimationDuration {
     static let slow = 0.5
 }
