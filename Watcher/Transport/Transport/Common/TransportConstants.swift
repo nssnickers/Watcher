3a18240770c08be0bf912bacfea55b3ef28bb63d
Rita Sukhova <nadobnomars@gmail.com>
2019-02-22 11:07:20 +0300

    Добавлена документация к константам таргета Транспорт

--- Watcher/Transport/Transport/Common/TransportConstants.swift
+++ Watcher/Transport/Transport/Common/TransportConstants.swift
@@ -8,34 +8,67 @@
 
 import Foundation
 
+/// Ключи http заголовков
 struct HttpHeaderKey {
+    
+    /// Тип http контента
     static let contentType = "Content-Type"
+    
+    
+    /// Cookie для установки
     static let setCoookie = "Set-Cookie"
+    
 }
 
 
+/// Тип http контента
 struct HttpContentType {
+    
+    /// JSON
     static let json = "application/json"
+    
 }
 
 
+/// Запросы к серверу
 struct Api {
+    
+    /// Базовый URL сервера
     struct Url {
+        
+        /// Схема
         static let scheme = "https"
         
+        /// Хост
         static let host = "watcher.intern.redmadrobot.com"
-        static let path = "/api/v1/"
-    }
         
+        /// Путь
+        static let path = "/api/v1/"
+        
+    }
+    
+    
+    /// Запросы на аутентификацию
     struct Auth {
+        /// Аутентификация стажеров
         static let outstaff = "auth/sign-in/"
+        
     }
     
+    
+    /// Запросы для работы со списанным временем
     struct WorkTime {
+        
+        /// Списать часы
         static let log = "logged-time/"
+        
     }
     
+    
+    /// Запросы для работы с проектами
     struct Project {
+        
+        /// Получить список проектов
         static let obtainAll = "projects/"
     }
 }
